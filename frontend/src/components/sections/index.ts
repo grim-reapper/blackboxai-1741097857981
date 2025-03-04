b41b97ef581f507bb194997a1a956c3e0001a8ea
Imran Ali <imran.ali125@yahoo.com>
2025-03-04 19:19:02 +0500

    Add frontend/src/components/sections/index.ts 1741097941204

---
+++ frontend/src/components/sections/index.ts
@@ -0,0 +1,5 @@
+export { default as HeroSection } from './HeroSection';
+export { default as FeaturesSection } from './FeaturesSection';
+export { default as HowItWorksSection } from './HowItWorksSection';
+export { default as PricingSection } from './PricingSection';
+export { default as AboutSection } from './AboutSection';
